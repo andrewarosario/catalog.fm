6158a8e283d989e325b8ee0e855d76cd22a572e7
Andrew Rosário <andrew.arosario@gmail.com>
2020-06-13 01:18:03 -0300

    criando ScrobbleMessageService

---
+++ src/app/shared/services/usecases/scrobble-message.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { MessageService } from '@shared/services/message.service';
+import { ScrobbleResponseType } from '@core/models/scrobble-response-type';
+import { ScrobbleService } from './scrobble.service';
+import { tap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+
+const MESSAGE_SCROBBLE = {
+  [ScrobbleResponseType.Lastfm]: 'Faixa scrobblada com sucesso!',
+  [ScrobbleResponseType.IndexedDb]: 'A faixa foi armazenada no cache'
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScrobbleMessageService {
+
+  constructor(
+    private scrobbleService: ScrobbleService,
+    private messageService: MessageService
+  ) { }
+
+  public scrobble(track: TrackScrobble) {
+    return this.scrobbleService.scrobble(track).pipe(
+      tap(response => this.messageService.open(MESSAGE_SCROBBLE[response])),
+      catchError(err => of(this.messageService.open('Erro ao scrobblar faixa!')))
+    );
+  }
+}
